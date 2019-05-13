e52528db7982bd60aff0eefc316f4b7339271748
jean5217q <jean5217q@gmail.com>
2019-05-13 13:45:12 +0800

    update

--- src/component/loading/Background_Loading.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Background_Loading extends Component {
-  render(){
-    return(<div className="background-loading"></div>)
-  }
-}
-
-export default Background_Loading
